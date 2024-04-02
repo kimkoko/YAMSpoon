bd3a96a0c1c2caf532e11f8988e24a949b2171a6
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-04-02 12:17:39 +0900

    🎉 Init: 프로젝트 시작
    폴더 구성

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './assets/App.scss';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MaterialRecipe from './pages/material/MaterialRecipe';
+import Refrigerator from './pages/material/Refrigerator';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/recipe/Search';
+import TypeRecipe from './pages/recipe/TypeRecipe';
+import EditProfile from './pages/user/EditProfile';
+import FindId from './pages/user/FindId';
+import FindPassword from './pages/user/FindPassword';
+import MyPage from './pages/user/MyPage';
+import SignIn from './pages/user/SignIn';
+import SignUp from './pages/user/SignUp';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/material-recipe" element={<MaterialRecipe />} />
+        <Route path="/regrigerator" element={<Refrigerator />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/type-recipe" element={<TypeRecipe />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/find-id" element={<FindId />} />
+        <Route path="/find-password" element={<FindPassword />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

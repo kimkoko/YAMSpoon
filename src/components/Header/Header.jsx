43b715461b80029008929431ff7f0e1c74b9902c
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-04-03 11:48:55 +0900

    💄 Design: select, pagination 추가 및 index.scss 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,18 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className="header">
-        <img className='logo' src={process.env.PUBLIC_URL + '/images/logo.svg'} alt='logo'/>
-        <div className='nav'>
-            <Link className='text' to = "/material">재료별 레시피</Link>
-            <Link className='text' to = "/type-recipe">종류별 레시피</Link>
-            <Link className='text' to = "/regrigerator">나만의 냉장고</Link>
-        </div>
-        <div className='searchContainer'>
-            <input className="search-cotainer" type="text" placeholder="무엇을 찾아드릴까요?"/>
-            <img className='search' src={process.env.PUBLIC_URL + '/images/search.svg'} alt='search' />
-        </div>
-        <img className='myPage' src={process.env.PUBLIC_URL + '/images/myPage.svg'} alt='mypage' />
-        <button className='login'>로그인</button>
+      <img className='logo' src={process.env.PUBLIC_URL + '/images/logo.svg'} alt='logo'/>
+      <div className='nav'>
+          <Link className='text' to = "/material">재료별 레시피</Link>
+          <Link className='text' to = "/type-recipe">종류별 레시피</Link>
+          <Link className='text' to = "/regrigerator">나만의 냉장고</Link>
+      </div>
+      <div className='searchContainer'>
+          <input className="search-cotainer" type="text" placeholder="무엇을 찾아드릴까요?"/>
+          <img className='search' src={process.env.PUBLIC_URL + '/images/search.svg'} alt='search' />
+      </div>
+      <img className='myPage' src={process.env.PUBLIC_URL + '/images/myPage.svg'} alt='mypage' />
+      <button className='login'>로그인</button>
     </header>
   );
 };
---
+++ src/pages/recipe/TypeRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TypeRecipe = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TypeRecipe;

40d5c4a061b4c54f25ad0eb2efc8d033a95fca09
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-04-04 19:19:40 +0900

    🐛 Fix: 대소문자 오류 해결

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './pagination.scss'
+import './agination.scss'
 import ArrowBackFilled from '../Icons/ArrowBackFilled';
 import ArrowForwardFilled from '../Icons/ArrowForwardFilled';
 

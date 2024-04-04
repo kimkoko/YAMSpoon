c2859301d716c46bcab32727db554a39ec0da3d4
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-04-04 18:19:28 +0900

    💄 Design: 종류별레시피, 검색페이지 추가 및 header, footer 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './assets/App.scss';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MaterialRecipe from './pages/material/MaterialRecipe';
 import Refrigerator from './pages/material/Refrigerator';
@@ -17,22 +17,20 @@ import SignUp from './pages/user/SignUp';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/material-recipe" element={<MaterialRecipe />} />
-        <Route path="/regrigerator" element={<Refrigerator />} />
-        <Route path="/recipe" element={<Recipe />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/type-recipe" element={<TypeRecipe />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/find-id" element={<FindId />} />
-        <Route path="/find-password" element={<FindPassword />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/material-recipe" element={<MaterialRecipe />} /> 
+      <Route path="/regrigerator" element={<Refrigerator />} /> 
+      <Route path="/recipe" element={<Recipe />} /> 
+      <Route path="/search" element={<Search />} /> 
+      <Route path="/type-recipe" element={<TypeRecipe />} /> 
+      <Route path="/edit-profile" element={<EditProfile />} />
+      <Route path="/find-id" element={<FindId />} />
+      <Route path="/find-password" element={<FindPassword />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   );
 }
 

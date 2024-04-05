a7b19380194db83c4ff245b679002e9ea2fc36b4
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-04-05 11:21:12 +0900

    🚑️ Fix: 오류 해결

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom'
-import { Link, useNavigate } from 'react-router-dom'
 import './Header.scss';
-import SearchIcon from '../Icons/SearchIcon';
-import MypageIcon from '../Icons/MypageIcon';
-import Logo from '../Icons/LogoIcon'
 import SearchIcon from '../Icons/SearchIcon';
 import MypageIcon from '../Icons/MypageIcon';
 import Logo from '../Icons/LogoIcon'
 
 const Header = () => {
-  const navigate = useNavigate();
-
-  const activeEnter = (e) => {
-    if(e.key === "Enter") {
-      navigate('/search');
-    }
-  }
-
   const navigate = useNavigate();
 
   const activeEnter = (e) => {
@@ -44,18 +32,7 @@ const Header = () => {
             <Link className="search-icon" to="/search"><SearchIcon alt ="searchIcon" /></Link>
           </div>
           <Link className='mypage-icon' to="/mypage"><MypageIcon alt="myPageIcon"/></Link>
-          <button className='login'>로그인</button>
-        </div>
-        </div>
-
-        <div className='right-container'>
-          <div className='searchContainer'>
-            <input className="search-cotainer" id='search' type="text" placeholder="무엇을 찾아드릴까요?"
-              onKeyDown={(e) => activeEnter(e)}/>
-            <Link className="search-icon" to="/search"><SearchIcon alt ="searchIcon" /></Link>
-          </div>
-          <Link className='mypage-icon' to="/mypage"><MypageIcon alt="myPageIcon"/></Link>
-          <button className='login'>로그인</button>
+          <Link to="/signin"><button className='login'>로그인</button></Link>
         </div>
       </div>
     </header>

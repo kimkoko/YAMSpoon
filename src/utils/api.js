ca2c0d9b958ca39436dbf7c6a46484f2c36fa041
Kang KyungRim <kkltr416@gmail.com>
2024-04-08 16:13:58 +0900

    ✨ Feat: api > axios 인터셉터 내용 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,37 @@
-// axios 설정 파일
-// 모든 컴포넌트들이 axios를 불러올때 사용하는 공간
-// 코드 채우기
+import axios from 'axios';
+
+// axios 인스턴스
+const instance = axios.create({
+    baseURL: 'https://localhost:3000',
+    timeout: 1000
+});
+
+// 요청
+instance.interceptors.request.use(
+    function (config) {
+        // 요청 성공 직전 호출됩니다.
+        // axios 설정값을 넣습니다. (사용자 정의 설정도 추가 가능)
+        config.headers["Content-Type"] = "application/json; charset=utf-8";
+        config.headers["Authorization"] = " 토큰 값";
+        return config;
+    }, 
+    function (error) {
+        // 요청 에러 직전 호출됩니다.
+        console.log(error)
+        return Promise.reject(error);
+    }
+);
+
+
+// 응답
+instance.interceptors.response.use(
+    function (response) {
+        console.log(response)
+        return response;
+    },
+
+    function (error) {
+        console.log(error)
+        return Promise.reject(error);
+    }
+);

95efc2c8eb49732050fe93cd6ec308f03daa1e04
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-04-09 15:34:30 +0900

    💄 Design: Carousel UI 수정 및 삭제기능 구현

--- src/pages/material/Refrigerator.jsx
+++ src/pages/material/Refrigerator.jsx
@@ -15,9 +15,8 @@ import AddModal from './AddModal'
 const Refrigerator = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const [ materials, setMaterials ] = useState(['감자', '계란']);
-    // const materials = ['감자', '계란', '양파', '당근', '오이', '김치', '고등어','꽁치']
-    const recipes = 9
+    const [ materials, setMaterials ] = useState(['감자', '계란','양파', '당근', '오이', '김치', '고등어','꽁치' ]);
+    const recipes = 4;
 
   const handleAddClick = (e) => {
         e.preventDefault();
@@ -27,7 +26,7 @@ const Refrigerator = () => {
 
     const handleDeleteMaterial = (index) => {
         const updatedMaterials = [...materials];
-        updatedMaterials.splice(index, 1);
+        updatedMaterials.splice(index, 1); // 올바른 splice 사용법으로 수정
         setMaterials(updatedMaterials);
     }
 
@@ -36,7 +35,7 @@ const Refrigerator = () => {
         <Header />
         <div className={styles['material-container']}>
             <div className={styles['title']}>냉장고 속 재료로 레시피가 준비되었어요!</div>
-            <MaterialBar handleAddClick={handleAddClick} materials={materials} deleteMaterial={handleDeleteMaterial} recipes={recipes} />
+            <MaterialBar handleAddClick={handleAddClick} materials={materials} handleDeleteMaterial={handleDeleteMaterial} recipes={recipes} />
             <div className={styles['result']}>
                 <p>검색 결과 <span>{recipes}</span>건 조회</p>
                 <select name="order">
@@ -56,13 +55,13 @@ const Refrigerator = () => {
   )
 }
 
-const MaterialBar = ({handleAddClick, materials, handleDeleteMaterial, recipes}) => {
+const MaterialBar = ({ handleAddClick, materials, handleDeleteMaterial, recipes }) => {
 
     return (
         <div className={styles['bar-container']}>
             <div className={styles['main-bar']}>
                 <button className={styles['all']}>전체</button>
-                <Carousel CategoryData={materials} items={recipes} showDeleteButton={true} handleDeleteMaterial = {handleDeleteMaterial}/>
+                <Carousel CategoryData={materials} items={recipes} showDeleteButton={true} deleteMaterial = {handleDeleteMaterial}/>
             </div>
             <button className={styles['Add']} onClick={handleAddClick}>재료 추가</button>
         </div>

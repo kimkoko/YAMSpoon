a78c812f4de5c78daacdff6a79020e9997ed5762
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-04-12 00:23:21 +0900

    ✨ Feat: 로그인 임시 기능 구현
    json-server 사용

--- src/pages/user/SignIn.jsx
+++ src/pages/user/SignIn.jsx
@@ -1,23 +1,75 @@
-import React from 'react'
-import LoginHeader from '../../components/Header/LoginHeader'
-import { Link } from 'react-router-dom'
-import './SignIn.scss'
+import React, { useState } from 'react';
+import LoginHeader from '../../components/Header/LoginHeader';
+import { Link, useNavigate } from 'react-router-dom';
+// import User from '../../utils/User';
+import { api } from '../../utils/api';
+import Modal from '../../components/Modal/Modal';
+import Alert from '../../components/Icons/Alert';
+import './SignIn.scss';
 
-const SignUp = () => {
+const SignIn = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    id: '',
+    password: ''
+  });
+  const [formError, setFormError] = useState('');
+  const [errorModalOpen, setErrorModalOpen] = useState(false);
+
+  const { id, password } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!id || !password) {
+      setFormError('※ 모든 필드를 입력해 주세요.');
+    } else {
+      setFormError('');
+      const res = await api.get('/user');
+      const user = res.data.find(user => user.userId === id && user.password === password);
+      if (user) {
+        alert(`로그인 성공! 안녕하세요 ${user.name}님`)
+        navigate('/');
+      } else {
+        setErrorModalOpen(true);
+      }
+    }
+  }
+
   return (
     <div>
       <LoginHeader />
       <div className='signin-container'>
         <p className='signin-title'>로그인</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor='id'>아이디</label>
-            <input id='id' type='text' placeholder='아이디를 입력해 주세요.' />
+            <input
+              id='id'
+              name='id'
+              type='text'
+              placeholder='아이디를 입력해 주세요.'
+              value={formData.id}
+              onChange={handleInputChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor='password'>비밀번호</label>
-            <input id='password' type='password' placeholder='비밀번호를 입력해 주세요.' />
+            <input 
+              id='password' 
+              name='password'
+              type='password' 
+              placeholder='비밀번호를 입력해 주세요.'
+              value={formData.password}
+              onChange={handleInputChange}
+            />
           </div>
+          {formError && <p className='error-message'>{formError}</p>}
           <button type='submit' className='signin-button long'>로그인</button>
         </form>
         <div className='link-container'>
@@ -29,8 +81,17 @@ const SignUp = () => {
           <Link to='/signup'>회원가입</Link>
         </div>
       </div>
+      {errorModalOpen && (
+        <Modal 
+          IconComponent={Alert}
+          alertBody='아이디 또는 비밀번호가 일치하지 않습니다.'
+          buttonAction={() => setErrorModalOpen(false)}
+          actionText='확인'
+          hideCloseButton={true}
+        />
+      )}
     </div>
   )
 }
 
-export default SignUp
+export default SignIn;

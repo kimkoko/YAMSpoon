ea33a937a9d13138cb840168a27b85612be5535b
Song I Kim <songikim@Songui-MacBookAir.local>
2024-04-12 17:17:05 +0900

    ✨ Feat: 재료별 페이지 기능
    카테고리, 재료 서버에서 가져오기, 레시피 선택조회/정렬

--- src/pages/material/MaterialBar.jsx
+++ src/pages/material/MaterialBar.jsx
@@ -1,65 +1,103 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
+import PropTypes from 'prop-types';
 import styles from './MaterialBar.module.scss'
 import Carousel from "../../components/Carousel/Carousel";
+import Ingredients from "../../utils/Ingredients";
 
-const MaterialBar = () => {
+const MaterialBar = ({ handleMaterialSelect, handleAllClick }) => {
     const [ selectedSub, setSelectedSub ] = useState(null);
+    const [ categoryData, setCategoryData ] = useState(null)
+    const [ subCategoryData, setSubCategoryData ] = useState(null)
+    const [ filteredSub, setFilteredSub ] = useState(null)
+
+    const carouselRef = useRef(null);
+
+    useEffect(() => {
+        const fetchCategory = async () => {
+            try {
+              const response = await Ingredients.getIngredientsCategory()
+              const categories = response.data.map(item => item.category);
+              setCategoryData(categories)
+      
+            } catch (error) {
+              console.error('Error fetching recipes:', error);
+            }
+        };
+
+        const fetchSubCategory = async() => {
+            try {
+                const response = await Ingredients.getIngredients()
+                const subCategory = response.data
+                setSubCategoryData(subCategory)
+            } catch (error) {
+                console.error('Error fetching subcat:', error)
+            }
+        }
+      
+        fetchCategory();
+        fetchSubCategory();
+    }, [])
 
     const handleSubClick = (idx) => {
         setSelectedSub(selectedSub === idx ? null : idx);
+        handleMaterialSelect(filteredSub[idx].id)
     };
-    
-    const categoryData = ['가공/유제품', '고기', '곡물', '과일', '면', '빵/떡', '채소', '콩/견과류', '해산물', '햄/소시지']
+
+    const handleSubSelect = (index) => {
+        const value = `c${index+1}`
+        const filteredArr = subCategoryData
+                            .filter(item => item.categoryid === value)
+        setFilteredSub(filteredArr)
+        setSelectedSub(null)
+    }
+
+    const handleAllButtonClick = () => {
+        handleAllClick()
+        setSelectedSub(null)
+        setFilteredSub(null)
+        if (carouselRef.current && carouselRef.current.handleSelect) {
+            carouselRef.current.handleSelect(-1);
+        }
+    }
+   
     const items = 7;
 
     return (
         
         <div className={styles['bar-container']}>
             <div className={styles["button-box"]}>
-                <button className={styles["all"]}>전체</button>
-
+                <button className={styles["all"]} onClick={handleAllButtonClick}>전체</button>
                 <div className={styles['main-bar']}>
-                    <Carousel CategoryData={categoryData} items={items} showDeleteButton={false} />
+                    {categoryData && <Carousel 
+                                        ref={carouselRef} 
+                                        CategoryData={categoryData} 
+                                        items={items} 
+                                        showDeleteButton={false} 
+                                        handleSubSelect={handleSubSelect} />}
                 </div>
             </div>
                 
             <div className={styles['sub-bar']}>
-                {materials[3].subcategories.map((subcategory, idx) => (
-                    <button 
-                        key={`0-${subcategory}`}
+                { filteredSub && filteredSub.map((item, idx) => (
+                    <button
+                        key={`${idx}-${item}`}
                         className={selectedSub === idx ? styles['selected'] : ''}
                         onClick={() => handleSubClick(idx)}
                     >
-                    {subcategory}
+                    {item.name}
                     </button>
                 ))}
+
             </div>
             
         </div>
     )
 }
 
-const materials = [
-    {
-        id: 1,
-        name: '육류',
-        subcategories: ['소고기', '돼지고기', '닭고기']
-    },
-    {
-        id: 2,
-        name: '유제품',
-        subcategories: ['우유', '치즈', '요거트', '요구르트', '크림(생크림)']
-    },
-    {
-        id: 3,
-        name: '과일',
-        subcategories: ['사과', '배', '바나나', '오렌지', '수박', '토마토', '아보카도']
-    },
-    {
-        id: 4,
-        name: '채소',
-        subcategories: ['시금치', '대파','양파', '당근']
-    },
-]
+MaterialBar.propTypes = {
+    handleMaterialSelect: PropTypes.func,
+    handleAllClick: PropTypes.func,
+    material: PropTypes.bool
+}
 
 export default MaterialBar

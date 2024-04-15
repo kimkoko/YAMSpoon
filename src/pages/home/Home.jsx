17c0975dd741df1f2677c1eeba2007c3c8b0a690
Song I Kim <songikim@Songui-MacBookAir.local>
2024-04-15 14:57:49 +0900

    💄 Desgin: 최근 레시피 10개로 수정, padding값 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import React,{useEffect,useState} from 'react'
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import './HomeSlideArrows.scss'
 import './Home.scss'
-import Heart from '../../components/Icons/Heart'
+//import Heart from '../../components/Icons/Heart'
 import TopButton from '../../components/TopButton/TopButton'
 import Cutlery from '../../components/Icons/Cutlery'
 import { Link } from 'react-router-dom'
@@ -19,9 +19,10 @@ const Home = () => {
   const fetchRecipes = async () => {
     try {
       const response = await Recipe.getRecipe()
+      console.log(response)
       //const slideDataDeepCopy = _.cloneDeep(response.data)
       const newestData = response.data.toSorted((a,b) => new Date(b.createdAt) - new Date(a.createdAt))
-      setNewestData(newestData.slice(0,4))
+      setNewestData(newestData.slice(0,10))
 
       const sortedData = response.data.toSorted((a, b) => b.user_like.length - a.user_like.length)
       const slicedData = sortedData.slice(0, 10);
@@ -59,9 +60,10 @@ const Home = () => {
 
         <div className='middle-line'></div>
         
-        <div className='recipe-container'>
+        <div className='recipe-container' style={{ paddingBottom: "82px"}}>
           <p className='title'>최근에 올라온 레시피는 어떤가요?</p>
-          {newestData && <RecipeList recipes={newestData}/>}
+          {/* {newestData && <RecipeList recipes={newestData}/>} */}
+          { newestData && <ImageCarousel slideDatas={newestData} hideRecipeRanking={true} />}
         </div>
 
         <TopButton />
@@ -70,33 +72,33 @@ const Home = () => {
   )
 }
 
-const RecipeList = ({ recipes }) => {
-  return (
-    <div className='recipe-items2'>
-      {recipes.map((recipe, index) => (
-          <div className='recipe-item' key={index}>
-            <Link to={`/recipes/${recipe.id}`}>
-            <div className='recipe-img'>
-              <img src={recipe.img} alt={`image_${index + 1}`} />
-            </div>
-            <p>{recipe.name}</p>
-            <span>
-              <Heart fill={"#D3233A"} />
-              {recipe.user_like.length}
-            </span>
-            </Link>
-        </div>
-      ))}
-    </div>
-  );
-};
+// const RecipeList = ({ recipes }) => {
+//   return (
+//     <div className='recipe-items2'>
+//       {recipes.map((recipe, index) => (
+//           <div className='recipe-item' key={index}>
+//             <Link to={`/recipes/${recipe.id}`}>
+//             <div className='recipe-img'>
+//               <img src={recipe.img} alt={`image_${index + 1}`} />
+//             </div>
+//             <p>{recipe.name}</p>
+//             <span>
+//               <Heart fill={"#D3233A"} />
+//               {recipe.user_like.length}
+//             </span>
+//             </Link>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-RecipeList.propTypes = {
-  recipes: PropTypes.arrayOf(PropTypes.shape({
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    user_like: PropTypes.array.isRequired,
-  })).isRequired,
-};
+// RecipeList.propTypes = {
+//   recipes: PropTypes.arrayOf(PropTypes.shape({
+//     img: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     user_like: PropTypes.array.isRequired,
+//   })).isRequired,
+// };
 
 export default Home

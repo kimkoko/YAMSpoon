e798ecf7713a111e9982f2af6a4af2b64dba99c7
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-04-16 12:23:17 +0900

    ✨ Feat: 아이디 찾기 api 연결
    아이디 찾기 페이지 기능 구현

--- src/pages/user/FindId.jsx
+++ src/pages/user/FindId.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import LoginHeader from '../../components/Header/LoginHeader';
+import User from '../../utils/User';
 import { validateEmptyFields } from '../../utils/validateEmptyFields';
 import Modal from '../../components/Modal/Modal';
 import FindIdIcon from '../../components/Icons/FindIdIcon';
 import './FindId.scss';
 
 const FindId = () => {
+  const navigate = useNavigate();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [formData, setFormData] = useState({
@@ -13,23 +17,27 @@ const FindId = () => {
     email: '',
   });
   const [emailCode, setEmailCode] = useState('');
+  const [userId, setUserId] = useState('');
 
   const [validation, setValidation] = useState({
     nameError: '',
     emailError: '',
     emailSend: false,
     emailCodeError: '',
+    emailCodeCheck: false,
   });
 
   const { name, email } = formData;
-  const { nameError, emailError, emailSend, emailCodeError } = validation;
+  const { nameError, emailError, emailSend, emailCodeError, emailCodeCheck } = validation;
 
+  // 입력 처리
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    setValidation({ ...validation, [`${name}Error`]: '' });
+    setValidation({ ...validation, [`${name}Error`]: '', [`${name}Check`]: '' });
   };
 
+  // 이메일 입력 처리
   const handleEmailChange = (e) => {
     const newEmail = e.target.value;
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -40,24 +48,44 @@ const FindId = () => {
     })
   };
 
+  // 이메일로 인증번호 전송(인증 버튼)
   const handleEmailSend = async () => {
     setValidation({ ...validation, emailSend: true, emailError: '' });
-    // 이메일 인증번호 전송
+    await User.sendEmailCode({ email });
   };
 
+  // 인증번호 입력 처리
   const handleEmailCodeChange = (e) => {
     const newEmailCode = e.target.value;
-    setEmailCode(newEmailCode);
+    setEmailCode(newEmailCode.slice(0, 6));
+    setValidation({ ...validation, emailCodeError: '', emailCodeCheck: false })
   }
 
+  // 이메일 인증번호 확인(확인 버튼)
   const handleEmailCodeConfirm = async () => {
-    // 이메일 인증번호 확인
     if (!emailCode) {
       setValidation({ ...validation, emailCodeError: '※ 인증번호를 입력해 주세요.' })
+    } else {
+      try {
+        const res = await User.verifyCode({ "verificationCode": emailCode });
+        
+        if (res.status === 200) {
+          setValidation({ ...validation, emailCodeError: '', emailCodeCheck: true });
+        }
+
+      } catch (err) {
+        if (err.response.status === 400) {
+          setValidation({ ...validation, 
+            emailCodeError: 'X 입력하신 인증번호가 틀렸습니다. 다시 확인해주세요.',
+            emailCodeCheck: false
+          });
+        }
+      }
     }
   }
 
-  const handleSubmit = (e) => {
+  // 아이디 찾기 폼 제출
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     // 빈 값 확인
@@ -65,11 +93,18 @@ const FindId = () => {
       return;
     }
 
-    // 이메일 인증 확인
+    // 이메일 인증 유무
     if (!emailSend) {
       setValidation({ ...validation, emailError: '※ 이메일 인증을 해주세요.' });
       return;
+    } else if (!emailCodeCheck) {
+      setValidation({ ...validation, emailCodeError: '※ 이메일 인증을 해주세요.' });
+      return;
     }
+
+    // 아이디 찾기 처리
+    const res = await User.findUserId(formData);
+    setUserId(res.data.data.slice(0, -3) + '***');
 
     setIsModalOpen(true);
   }
@@ -104,6 +139,7 @@ const FindId = () => {
                 value={email}
                 onChange={handleEmailChange}
                 className={`${emailError && 'invalid'}`}
+                disabled={emailSend}
               />
               <button
                 type="button"
@@ -118,16 +154,25 @@ const FindId = () => {
               <div className="input-with-button">
                 <input
                   type="number"
-                  placeholder="인증번호를 입력해 주세요."
+                  placeholder="인증번호 6자리를 입력해 주세요."
                   value={emailCode}
                   onChange={handleEmailCodeChange}
                   name="emailCode"
+                  disabled={emailCodeCheck}
                 />
-                <button type="button" className="notFilled short" onClick={handleEmailCodeConfirm}>확인</button>
+                <button
+                  type="button"
+                  className="notFilled short"
+                  onClick={handleEmailCodeConfirm}
+                  disabled={emailCodeCheck}
+                >
+                  확인
+                </button>
               </div>
             )}
             {emailError && <p className="error-message">{emailError}</p>}
             {emailCodeError && <p className="error-message">{emailCodeError}</p>}
+            {emailCodeCheck && <p className="success-message">O 인증번호가 일치합니다.</p>}
           </div>
           <button type='submit' className='findid-button long'>아이디 찾기</button>
         </form>
@@ -135,8 +180,8 @@ const FindId = () => {
       {isModalOpen && (
         <Modal
           IconComponent={FindIdIcon}
-          alertBody='ㅇㅇㅇ님의 아이디는 eli**** 입니다.'
-          buttonAction={() => setIsModalOpen(false)}
+          alertBody={`${name}님의 아이디는 ${userId} 입니다.`}
+          buttonAction={() => navigate('/signin')}
           actionText='확인'
           hideCloseButton={true}
         />
